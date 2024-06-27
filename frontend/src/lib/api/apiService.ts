c92b4f4d344b5fa779623ad4b070c5b64b67adba
Munnazzah Aslam <munnazzah.aslam@nymcard.com>
2024-06-27 14:54:03 +0500

    update endpoint of APIs

--- frontend/src/lib/api/apiService.ts
+++ frontend/src/lib/api/apiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://personageniusapi.onrender.com";
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
